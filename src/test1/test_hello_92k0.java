72baa62a6c85433cd762a4e4c3a4738dfb6483c6
yikyoung <dowei22@naver.com>
2022-03-07 14:21:42 +0900

    fdg

--- src/test1/test_hello_92k0.java
+++ src/test1/test_hello_92k0.java
@@ -3,8 +3,8 @@ package test1;
 public class test_hello_92k0 {
 	public static void main(String[] args) {
 
-		System.out.println("Hello 92");
-		System.out.println("Hello 92");
+		System.out.println("Hello 902");
+		System.out.println("Hello 902");
 
 }
 }
