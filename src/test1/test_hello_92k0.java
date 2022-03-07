3491dca331d28af5ac7c9946ff3c928738ff448d
yikyoung <dowei22@naver.com>
2022-03-07 14:32:55 +0900

    xcv

--- src/test1/test_hello_92k0.java
+++ src/test1/test_hello_92k0.java
@@ -3,8 +3,8 @@ package test1;
 public class test_hello_92k0 {
 	public static void main(String[] args) {
 
-		System.out.println("Hello 92k0");
-		System.out.println("Hello 92k0");
+		System.out.println("Hello 92k0==");
+		System.out.println("Hello 92k0==");
 
 }
 }
